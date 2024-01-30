2084274339634945a50f80eca92aa411c22e4612
kaj33 <kaj33@mecbuc-kage33.homenet.telecomitalia.it>
2024-01-30 10:09:53 +0100

    seconda modifica

--- src/gestionefile/Lettore.java
+++ src/gestionefile/Lettore.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
         try {
